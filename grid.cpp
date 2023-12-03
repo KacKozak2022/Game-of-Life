083936214667028d01c37637821d8c6afe2fc7fa
Kamil-Klecz <kk305373@student.polsl.pl>
2023-12-03 19:18:32 +0100

    update class grid

--- grid.cpp
+++ grid.cpp
@@ -8,8 +8,11 @@ grid::grid() noexcept
 
 }
 
-void allocate(int height, int width){
-
-
+void grid::allocate(int height, int width)
+{
+    m_tab = new char* [height+2];
+    for(int i = 0;i < height+2; i++){
+        m_tab[i] = new char [width+2];
+    }
 
 }
--- grid.h
+++ grid.h
@@ -5,21 +5,14 @@
 class grid
 {
     Q_OBJECT
-    int height = 0;
-    int width = 0;
-    char** tab[];
-
-
-
+    int m_height = 0;
+    int m_width = 0;
+    char** m_tab;
 
 public:
     grid() noexcept;
-
-
     void allocate(int height, int width);
-
-
-
+    void deallocate();
 
 };
 
