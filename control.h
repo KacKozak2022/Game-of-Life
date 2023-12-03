29df525bdf5d2b066bfb3f5935f6c5b2789f0201
Kamil-Klecz <kk305373@student.polsl.pl>
2023-12-03 20:31:39 +0100

    add class control

---
+++ control.h
@@ -0,0 +1,15 @@
+#ifndef CONTROL_H
+#define CONTROL_H
+#include <QMainWindow>
+#include "grid.h"
+
+class Control
+{
+    Q_OBJECT
+    Grid grid;
+
+public:
+    Control();
+};
+
+#endif // CONTROL_H
