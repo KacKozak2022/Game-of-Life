1e5d88917f271899e07b0983c607efa9fbd8976d
KacKozak2022 <kk305378@student.polsl.pl>
2023-12-17 17:22:45 +0100

    add methods for simulation

--- control.cpp
+++ control.cpp
@@ -1,5 +1,4 @@
 #include "control.h"
-#include <algorithm>
 #include <random>
 
 Control::Control(){}
@@ -22,8 +21,8 @@ short Control::checkState(int row, int col){
     return false;
 }
 
-void Control::iterate(){
-    //Zakresy pętli uwzględniają faktyczną wielkość tablicy (ramkę)
+void Control::iterate(){ //Zakresy pętli uwzględniają faktyczną wielkość tablicy (ramkę)
+
     for (size_t i = 1; i < m_height + 1; i++)
     {
         for (size_t j = 1; j < m_width + 1; j++)
@@ -31,6 +30,7 @@ void Control::iterate(){
             gridNext[i][j] = checkState(i, j);
         }
     }
+
     for (size_t i = 1; i < m_height + 1; i++)
     {
         for (size_t j = 1; j < m_width + 1; j++)
@@ -43,6 +43,7 @@ void Control::iterate(){
             }
         }
     }
+
     m_iterations++;
 }
 
@@ -50,10 +51,13 @@ void Control::ChangeDimensions(int height, int width){
 
     char** tempGridCurrent = gridCurrent;
     bool** tempGridNext = gridNext;
-    allocGrid<char>(gridCurrent, height, width);
-    allocGrid<bool>(gridNext, height, width);
+
+    allocGrid(gridCurrent, height, width);
+    allocGrid(gridNext, height, width);
+
     int minHeight = min(height, m_height);
     int minWidth = min(width, m_width);
+
     for (size_t i = 1; i < minHeight; i++)
     {
         for (size_t j = 1; j < minWidth; j++)
@@ -62,8 +66,10 @@ void Control::ChangeDimensions(int height, int width){
             gridNext[i][j] = tempGridNext[i][j];
         }
     }
-    deallocGrid<char>(tempGridCurrent);
-    deallocGrid<bool>(tempGridNext);
+
+    deallocGrid(tempGridCurrent);
+    deallocGrid(tempGridNext);
+
     m_width = width;
     m_height = height;
 }
@@ -95,15 +101,15 @@ void Control::loadFromFile()
     else
     {
         QFile file(fileName);
-        file.close();
 
         if(file.open(QIODevice::ReadOnly))
         {
-            deallocGrid<char>(gridCurrent);
-            deallocGrid<bool>(gridNext);
+            deallocGrid(gridCurrent);
 
             QTextStream stream(&file);
             stream >> m_height >> m_width;
+
+            allocGrid(gridCurrent, m_height, m_width);
 
             for(int i = 0; i < m_height+2; i++)
             {
@@ -130,7 +136,6 @@ void Control::saveToFile()
     else
     {
         QFile file(fileName);
-        file.close();
 
         if(file.open(QIODevice::WriteOnly))
         {
@@ -151,7 +156,45 @@ void Control::saveToFile()
     }
 }
 
+void Control::simStart(int duration){
+    m_isSimRunning = 1;
+    m_isSimPaused = 0;
 
+    if(m_simTimeRemaining){
+        duration = m_simTimeRemaining;
+    }
+    QTime time = QTime::currentTime();
+    int timeBegin = time.second();
+
+    while(time.second() - timeBegin < duration)
+    {
+        qApp->processEvents();
+        time = QTime::currentTime();
+        iterate();
+        Sleep(250);
+
+        if(!(m_isSimRunning))
+        {
+            if(m_isSimPaused)
+                m_simTimeRemaining = duration - (time.second() - timeBegin);
+            break;
+        }
+    }
+    if(m_isSimRunning)
+    {
+        m_simTimeRemaining = 0;
+        m_isSimRunning = 0;
+    }
+}
+
+void Control::simPause(){
+    m_isSimRunning = 0;
+    m_isSimPaused = 1;
+}
+
+void Control::simStop(){
+    m_isSimRunning = 0;
+}
 
 
 
--- control.h
+++ control.h
@@ -4,6 +4,10 @@
 #include <QFile>
 #include <QFileDialog>
 #include <QMessageBox>
+#include <QApplication>
+#include <QTime>
+#include <QDebug>
+#include <windows.h>
 
 using namespace std;
 
@@ -16,30 +20,41 @@ class Control: public QObject
     int m_iterations = 0;
     int m_seed = 0;
 
+    bool m_isSimRunning = 0;
+    bool m_isSimPaused = 0;
+    int m_simTimeRemaining = 0;
+
     char** gridCurrent = nullptr;
     bool** gridNext = nullptr;
 
     short checkState(int row, int col);
 
     template<typename type>
-    void deallocGrid(type** tab){
-          if(tab != nullptr){
-              for(int i = 0;i < m_height+2; i++){
-                  delete[] tab[i];
-                  tab[i]=nullptr;
-              }
-              delete[] tab;
-              tab = nullptr;
+    void deallocGrid(type** tab)
+    {
+        if(tab != nullptr)
+        {
+          for(int i = 0;i < m_height+2; i++)
+          {
+              delete[] tab[i];
+              tab[i]=nullptr;
           }
-      }
+          delete[] tab;
+          tab = nullptr;
+        }
+    }
 
     template<typename type>
-    void allocGrid(type** tab, int height, int width){
-            tab = new type* [height+2];
-            for(int i = 0;i < height+2; i++){
+    void allocGrid(type** tab, int height, int width)
+    {
+        tab = new type* [height+2];
+
+        for(int i = 0;i < height+2; i++)
+        {
             tab[i] = new type [width+2];
         }
     }
+
 public:
     Control();
     void ChangeDimensions(int height, int width);
@@ -47,6 +62,9 @@ public:
     void seed(unsigned int entered_seed);
     void loadFromFile();
     void saveToFile();
+    void simStart(int duration);
+    void simPause();
+    void simStop();
 };
 
 #endif // CONTROL_H
